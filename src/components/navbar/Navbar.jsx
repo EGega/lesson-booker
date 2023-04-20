d891ac32d3fd8c913157993ff46014683d0f65f0
gegaenea@yahoo.com <gegaenea@yahoo.com>
2023-04-20 03:57:03 +0300

    Navbar and Logout button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import eneaPic from "../../assets/enea.jpg"
+import { NavbarStyle } from '../styled/navbarStyled/navbar'
+import { Link } from 'react-router-dom'
+import styles from "./Navbar.module.css"
+import { LoginBtn } from '../styled/LoginStyle.styled'
 const Navbar = () => {
   return (
-    <div>
-      <h4>Welcome Teacher Enea</h4>
-      <img src={eneaPic} alt="username will come here" />
-    </div>
+    <NavbarStyle>
+     <div className={styles.linkDiv}>
+      <li><Link to="/">Profile</Link></li>
+      <li><Link to="/">Lessons</Link></li>
+      <li><Link to="/">Calendar</Link></li>
+     </div>
+     <div className={styles.imgDiv}>
+      <LoginBtn><Link to="/login">Log Out</Link></LoginBtn>
+      <Link to="/">Teacher Enea</Link>
+      <img className={styles.avatarImage} src={eneaPic} alt="username will come here" />
+     </div>
+    </NavbarStyle>
+
 
   )
 }
--- src/components/styled/navbarStyled/navbar.js
+++ src/components/styled/navbarStyled/navbar.js
@@ -1,6 +1,13 @@
 import styled from "styled-components"
 
 export const NavbarStyle = styled.nav `
-
-
+margin-top: 10px;
+position: fixed;
+width: 100%;
+top: 0;
+left: 0;
+display:flex;
+font-size: 20px;
+align-items: center;
+justify-content: space-between;
 `
