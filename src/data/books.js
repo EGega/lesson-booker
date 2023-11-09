4e23bb2a23a2345be3f12d0a300f36dc4b033751
Enea <gegaenea@yahoo.com>
2023-11-10 00:30:17 +0300

    Ai Generated Books added and books array created

--- src/data/books.js
+++ src/data/books.js
@@ -1,4 +1,11 @@
 import businessBeginner from "../assets/books/businessBeginner.png" 
+import businessIntermediate from "../assets/books/businessIntermediate.jpg" 
+import businessAdvanced from "../assets/books/businessAdvanced.png" 
+import englishPhonics from "../assets/books/englishPhonics.png" 
+import englishOral from "../assets/books/englishOral.png" 
+import englishFirstGrade from "../assets/books/englishFirstGrade.png" 
+import englishSecondGrade from "../assets/books/englishSecondGrade.png" 
+import englishThirdGrade from "../assets/books/englishThirdGrade.png" 
 export const books = [
 {    id: 1,
     title: "Business English Beginner ",
@@ -12,14 +19,49 @@ export const books = [
     pbhouse: "Life of Pi",
     author: "Jenny and Jase Mercury",
     freeTrial: true,
-    image: businessBeginner
+    image: businessIntermediate
 },
 {   id: 3,
-    title: "Business English Intermediate ",
+    title: "Business English Advanced ",
     pbhouse: "Life of Pi",
     author: "Jenny and Jase Mercury",
     freeTrial: false,
-    image: businessBeginner
+    image: businessAdvanced
+},
+{   id: 4,
+    title: "Spoken English ",
+    pbhouse: "HoksFoord Publications",
+    author: "Jeremy Jenkins",
+    freeTrial: true,
+    image: englishOral
+},
+{   id: 5,
+    title: "English Phonics",
+    pbhouse: "Phoenix House",
+    author: "Phillip Philly",
+    freeTrial: false,
+    image: englishPhonics
+},
+{   id: 6,
+    title: "English First Grade ",
+    pbhouse: "Kampridge",
+    author: "Coney McGregor",
+    freeTrial: true,
+    image: englishFirstGrade
+},
+{   id: 7,
+    title: "English Second Grade ",
+    pbhouse: "Kampridge",
+    author: "Coney McGregor",
+    freeTrial: true,
+    image: englishSecondGrade
+},
+{   id: 8,
+    title: "English Third Grade ",
+    pbhouse: "Kampridge",
+    author: "Coney McGregor",
+    freeTrial: true,
+    image: englishThirdGrade
 },
 
 ]
