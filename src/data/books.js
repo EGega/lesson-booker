26bd03c76d54a6c9075d382ec76b004dc8b62b99
Enea <gegaenea@yahoo.com>
2023-11-13 01:12:49 +0300

    Load component removed

--- src/data/books.js
+++ src/data/books.js
@@ -63,5 +63,61 @@ export const books = [
     freeTrial: true,
     image: englishThirdGrade
 },
+{    id: 9,
+    title: "Business English Beginner ",
+    pbhouse: "Life of Pi",
+    author: "Jenny and Jase Mercury",
+    freeTrial: true,
+    image: businessBeginner
+},
+{   id: 10,
+    title: "Business English Intermediate ",
+    pbhouse: "Life of Pi",
+    author: "Jenny and Jase Mercury",
+    freeTrial: true,
+    image: businessIntermediate
+},
+{   id: 11,
+    title: "Business English Advanced ",
+    pbhouse: "Life of Pi",
+    author: "Jenny and Jase Mercury",
+    freeTrial: false,
+    image: businessAdvanced
+},
+{   id: 12,
+    title: "Spoken English ",
+    pbhouse: "HoksFoord Publications",
+    author: "Jeremy Jenkins",
+    freeTrial: true,
+    image: englishOral
+},
+{   id: 13,
+    title: "English Phonics",
+    pbhouse: "Phoenix House",
+    author: "Phillip Philly",
+    freeTrial: false,
+    image: englishPhonics
+},
+{   id: 14,
+    title: "English First Grade ",
+    pbhouse: "Kampridge",
+    author: "Coney McGregor",
+    freeTrial: true,
+    image: englishFirstGrade
+},
+{   id: 15,
+    title: "English Second Grade ",
+    pbhouse: "Kampridge",
+    author: "Coney McGregor",
+    freeTrial: true,
+    image: englishSecondGrade
+},
+{   id: 16,
+    title: "English Third Grade ",
+    pbhouse: "Kampridge",
+    author: "Coney McGregor",
+    freeTrial: true,
+    image: englishThirdGrade
+},
 
 ]
