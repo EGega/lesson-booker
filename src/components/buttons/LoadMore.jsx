4bfb51fdb35a73acd436eeffdef0f50a8dbe2e40
Enea <gegaenea@yahoo.com>
2023-11-13 01:00:58 +0300

    Load More Books Logic Added

---
+++ src/components/buttons/LoadMore.jsx
@@ -0,0 +1,10 @@
+import styled from "./LoadMore.module.css"
+const LoadMore = () => {
+    return (
+        <div>
+            <button  className={styled.btn}>Load More</button>
+        </div>
+    )
+}
+
+export default LoadMore
--- src/pages/teacherPages/books/Books.jsx
+++ src/pages/teacherPages/books/Books.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import {books} from "../../../data/books.js"
-import styled from "./Books.module.css"
+import React, { useState } from 'react';
+import { books } from "../../../data/books.js";
+import styled from "./Books.module.css";
+
 const Books = () => {
-    return (
-        <div className={styled.container}>
-        {books.map((book) => {
-         const {id, author, title, image, freeTrial, pbhouse} = book
-         return(
-        <div  key={id}>
-           <img className={styled.img}  src={image} alt="" />
-           <div>
-             <h2>{title}</h2>
-             <h2>{author}</h2>
-           </div>
-           <div>
-             <h3>{freeTrial}</h3>
-             <h3>{pbhouse}</h3>
-           </div>
-          
-         </div>
-         )
-        })
-         }
-        </div>
-    );
-}
+  const [count, setCount] = useState(4);
+
+  return (
+    <div className={styled.container}>
+      {books.slice(0, count).map((book) => {
+        const { id, author, title, image, freeTrial, pbhouse } = book;
+        return (
+          <div key={id}>
+            <img className={styled.img} src={image} alt="" />
+            <div>
+              <h2>{title}</h2>
+              <h2>{author}</h2>
+            </div>
+            <div>
+              <h3>{freeTrial}</h3>
+              <h3>{pbhouse}</h3>
+            </div>
+          </div>
+        );
+      })}
+      <button
+        onClick={() => {
+          setCount(count + 4);
+        }}
+        className={styled.btn}
+      >
+        Load More
+      </button>
+    </div>
+  );
+};
 
 export default Books;
