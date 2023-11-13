164dcf127c480d6280206f96a7b634b77d216d83
Enea <gegaenea@yahoo.com>
2023-11-13 22:54:43 +0300

    styling to the BookDetails page

--- src/pages/teacherPages/books/BookDetails.jsx
+++ src/pages/teacherPages/books/BookDetails.jsx
@@ -1,17 +1,26 @@
 import { useParams } from "react-router-dom"
 import { books } from "../../../data/books.js"
+import styled from "./BookDetails.module.css"
+import Navbar from '../../../components/navbar/Navbar.jsx';
 const BookDetails = () => {
     const {id} = useParams()
     const book = books.find((e) => e.id === parseInt(id))
-    console.log(book);
+    const {title, author, image, pbhouse, freeTrial } = book
     return (
-        <div>
-            <img src={book.image} alt="" />
-            <h3>{book.title}</h3>
-            <h4>{book.author}</h4>
-            <p>Publishing House: {book.pbhouse}</p>
-            <p>Free Trial: {book.freeTrial ? "Yes" : "No"}</p>
+        <>
+        <Navbar />
+            <div className={styled.container}>
+             <div className={styled.imgDiv}>
+             <img src={image} alt="" />
+            </div>
+            <div className={styled.infoDiv}>
+            <h3>{title}</h3>
+            <h4>{author}</h4>
+            <p>Publishing House: {pbhouse}</p>
+            <p> { freeTrial ? "Free Trial Available" : "No Free Trial"}</p>
+            </div>   
         </div>
+        </>
     )
 }
 
