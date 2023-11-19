888617e707e78ed37a6e1be081d2e9b2080672db
Enea <gegaenea@yahoo.com>
2023-11-19 23:49:38 +0300

    Profile component update css add

--- src/pages/teacherPages/profile/Profile.jsx
+++ src/pages/teacherPages/profile/Profile.jsx
@@ -1,9 +1,21 @@
-import LessonInfo from "../lessonInfo/LessonInfo"
+import eneaPic from "../../../assets/enea.jpg"
+import styled from "./Profile.module.css"
+import Navbar from "../../../components/navbar/Navbar"
 const Profile = () => {
   return (
-    <div>
-      <LessonInfo />
+    <>
+    <Navbar />
+    <div className={styled.container}>
+         <div className={styled.personalInfo}>
+         <img src={eneaPic}  className={styled.image} alt="" />
+          <h2 className={styled.genInfo}>Enea Gega</h2>
+          <h2 className={styled.genInfo}>English Teacher</h2>
+          <h2 className={styled.genInfo}>29 Years Old</h2>
+          <h2 className={styled.genInfo}>Albania</h2>
+          <h2 className={styled.genInfo}>TOEFL</h2>
+         </div>
     </div>
+    </>
   )
 }
 
