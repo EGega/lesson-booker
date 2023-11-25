57242c19263d9d5b00e0cd319ac84d47418fc051
Enea <gegaenea@yahoo.com>
2023-11-26 02:43:31 +0300

    react calendar replaced with big react calendar

--- src/pages/calendars/LessonCalendar.jsx
+++ src/pages/calendars/LessonCalendar.jsx
@@ -1,8 +1,18 @@
-import Calendar from "react-calendar";
+import { Calendar, globalizeLocalizer } from 'react-big-calendar'
+import globalize from 'globalize'
 
-const LessonCalendar = () => {
-    return (
-     <Calendar />
-    )
-}
+const localizer = globalizeLocalizer(globalize)
+
+const LessonCalendar = (props) => (
+  <div>
+    <Calendar
+      localizer={localizer}
+    //   events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+)
+
 export default LessonCalendar
