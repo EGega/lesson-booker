8689ed89cc26974cc1a1d8d91603e4ce91e20dd2
Enea <gegaenea@yahoo.com>
2023-12-02 19:29:27 +0300

    Changes made on Submitting handlers, revision urgently needed

---
+++ src/components/styled/styledbuttons/buttons.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const SubmitButton = styled.button `
+width: 40%;
+font-size: 1rem;
+padding: 10px;
+color: #fff;
+border: none;
+outline: none;
+border-radius: 10px;
+cursor: pointer;
+background-color: green;
+
+`
+export const ExitButton = styled.button `
+position: absolute;
+top: 10%;
+right: 5%;;
+background: none;
+outline: none;
+border: none;
+cursor: pointer;
+color: red;
+width: 35px;
+height: 35px;
+border-radius: 50%;
+font-weight: 600;
+font-size: 1.5rem;
+background-color: white;
+`
--- src/pages/calendars/LessonCalendar.jsx
+++ src/pages/calendars/LessonCalendar.jsx
@@ -5,7 +5,7 @@ import Navbar from "../../components/navbar/Navbar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import './LessonCalendar.css'
 import styled from "./LessonCalendar.module.css";
-import { SubmitButton } from "../../components/styled/styledbuttons/submitButton.js";
+import { SubmitButton, ExitButton } from "../../components/styled/styledbuttons/buttons.js";
 // import CalendarModule from "./calendarModule/CalendarModule";
 const localizer = momentLocalizer(moment);
 const LessonCalendar = () => {
@@ -14,7 +14,8 @@ const LessonCalendar = () => {
   const [module, setModule] = useState(false)
   const [events, setEvents] = useState([
   ]);
-
+  const [stName, setStName] = useState("")
+  const [timeSlotInfo, setTimeSlotInfo] = useState()
   // 
   const lessons = ["Maths", "English", "German", "Programming", "Robotics", "Drama"]
 
@@ -22,33 +23,57 @@ const LessonCalendar = () => {
   // Handlers
   const handleSelectSlot = (slotInfo) => {
     setModule(true)
+    setTimeSlotInfo(slotInfo)
+  };
+  const exitHandler = () => {
+    setModule(false)
+  }
+  // const submitHandler = (slotInfo) => {
+  //   setModule(false)
+  //   const newEvent = {
+  //     title: stName,
+  //     start: new Date(slotInfo.start),
+  //     end: new Date (slotInfo.end),
+  //   };
+  //   setEvents([...events, newEvent]);
+  //   console.log('Slot selected:', slotInfo);
+  // }
+  const submitHandler = (slotInfo) => {
+    setModule(false);
+  
+    const start = moment(slotInfo.start).startOf('minute').add(Math.ceil(moment(slotInfo.start).minute() / 30) * 30, 'minutes');
+    const end = moment(start).add(30, 'minutes');
+  
     const newEvent = {
-      title: 'New Event',
-      start: slotInfo.start,
-      end: slotInfo.end,
+      title: stName,
+      start: start.toDate(),
+      end: end.toDate(),
+      // lesson: lessons[lessonIndex],
+      studentName: stName,
     };
+  
     setEvents([...events, newEvent]);
     console.log('Slot selected:', slotInfo);
   };
-
-  const submithandler = (e) => {
-    if(e.key === 'Enter')
-    setModule(false)
-  }
-
+  
   return (
     <>
     {/* <Navbar /> */}
    {  module && <div className={styled.module}>
-            <input type="text" className={styled.nameInput} onKeyDown={submithandler} onChange={() => {}} placeholder='Enter Your Name' />
+            <input type="text" className={styled.nameInput} onChange={(e) => {
+              setStName(e.target.value)
+            }} placeholder='Enter Your Name' />
             <select name=""  id={styled.lessons}>
               {lessons.map((lesson, index) => {
                 return <option  key={index} value={lesson}>{lesson}</option>
               })}
             </select>
-            <SubmitButton>
+            <SubmitButton onClick={submitHandler(timeSlotInfo)}>
               Submit
             </SubmitButton>
+            <ExitButton onClick={exitHandler}>
+              X
+            </ExitButton>
     </div> }
     <div className={styled.app}>
       <Calendar
