a7421de90a198323189d16e0bed78ef81eab12f2
Enea <gegaenea@yahoo.com>
2023-12-02 18:37:25 +0300

    style added, options added for the lessons, styled components used for the submit button

---
+++ src/components/styled/styledbuttons/submitButton.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SubmitButton = styled.button `
+width: 40%;
+font-size: 1rem;
+padding: 10px;
+color: #fff;
+border: none;
+outline: none;
+border-radius: 10px;
+cursor: pointer;
+background-color: green;
+
+`
--- src/pages/calendars/LessonCalendar.jsx
+++ src/pages/calendars/LessonCalendar.jsx
@@ -5,12 +5,18 @@ import Navbar from "../../components/navbar/Navbar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import './LessonCalendar.css'
 import styled from "./LessonCalendar.module.css";
+import { SubmitButton } from "../../components/styled/styledbuttons/submitButton.js";
 // import CalendarModule from "./calendarModule/CalendarModule";
 const localizer = momentLocalizer(moment);
 const LessonCalendar = () => {
+
+  // state hook
   const [module, setModule] = useState(false)
   const [events, setEvents] = useState([
   ]);
+
+  // 
+  const lessons = ["Maths", "English", "German", "Programming", "Robotics", "Drama"]
 
 
   // Handlers
@@ -25,7 +31,8 @@ const LessonCalendar = () => {
     console.log('Slot selected:', slotInfo);
   };
 
-  const submithandler = () => {
+  const submithandler = (e) => {
+    if(e.key === 'Enter')
     setModule(false)
   }
 
@@ -33,16 +40,26 @@ const LessonCalendar = () => {
     <>
     {/* <Navbar /> */}
    {  module && <div className={styled.module}>
-            <input type="text" onKeyDown={submithandler} onChange={() => {}} placeholder='Enter Your Lesson' />
+            <input type="text" className={styled.nameInput} onKeyDown={submithandler} onChange={() => {}} placeholder='Enter Your Name' />
+            <select name=""  id={styled.lessons}>
+              {lessons.map((lesson, index) => {
+                return <option  key={index} value={lesson}>{lesson}</option>
+              })}
+            </select>
+            <SubmitButton>
+              Submit
+            </SubmitButton>
     </div> }
     <div className={styled.app}>
       <Calendar
         localizer={localizer}
         defaultDate={new Date()}
-        defaultView="week"
+        defaultView="day"
         events={events}
         selectable={true}
         onSelectSlot={handleSelectSlot}
+        // remove the drag and drop
+        onSelecting = {slot => false}
         step={30}
         timeslots={1}
         style={{ height: "100vh" }}
