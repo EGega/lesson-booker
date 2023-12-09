7d830d5438d874d0aecbd37d9e6e2d26ab9808dc
Enea <gegaenea@yahoo.com>
2023-12-09 23:39:41 +0300

    Card styling and info finished

--- src/pages/feedback/Feedback.jsx
+++ src/pages/feedback/Feedback.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import { teachers } from '../../data/teachersList'
 import styled from './Feedback.module.css'
-import { IoIosStar, IoIosStarHalf } from 'react-icons/io';
+import { IoIosStar, IoIosStarHalf, IoIosStarOutline } from 'react-icons/io';
 import femaleAvatar from "../../assets/femaleAvatar.png"
 import maleAvatar from "../../assets/maleAvatar.jpg"
-
 
 const Feedback = () => {
 
@@ -21,8 +20,8 @@ const Feedback = () => {
               <h3>{name}</h3>
               <h3>{new Date().getFullYear() - birthyear}</h3>
               <h3>{gender}</h3>
-              <h3><IoIosStar className={styled.fullStar}/></h3>
-              <h3><IoIosStarHalf className={styled.fullStar}/></h3>
+              <h3>Rate Your Teacher</h3>
+              <h3><IoIosStarOutline className={styled.emptyStar}/><IoIosStarOutline className={styled.emptyStar}/><IoIosStarOutline className={styled.emptyStar}/><IoIosStarOutline className={styled.emptyStar}/><IoIosStarOutline className={styled.emptyStar}/></h3>
            </div>
            )
        })}
