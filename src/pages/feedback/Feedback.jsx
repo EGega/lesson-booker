bc21fb00b79608778d6531473b56d8bc6d4cee3f
Enea <gegaenea@yahoo.com>
2023-12-12 00:45:43 +0300

    Calculation changed, still needs adjustements

--- src/pages/feedback/Feedback.jsx
+++ src/pages/feedback/Feedback.jsx
@@ -9,6 +9,7 @@ import { useState } from 'react';
 const Feedback = () => {
 const [rate, setRate] = useState(5)
 const [num, setNum] = useState(1)
+const [sum, setSum] = useState(0)
 const generateStars = (rating) => {
   const stars = [];
   for (let i = 1; i <= 5; i++) {
@@ -22,9 +23,13 @@ const generateStars = (rating) => {
 };
 
 const handleStar = (index) =>  {
-  setNum(() => num + 1)
-  setRate(() => (index + rate) / num )
-  console.log(rate);
+  setSum((prev) => prev + index)
+  console.log("The sum is", sum);
+  setNum((prevNum) => prevNum + 1)
+  console.log("The num is", num);
+  setRate((prevRate) => (sum + prevRate) / num);
+  console.log("The index is", index);
+  console.log("The rate is", rate);
 }
 
   return (
