a249c57190a31be2ee0abd84d8a44aad9c727bc8
Enea <gegaenea@yahoo.com>
2024-01-11 02:07:56 +0300

    main route page added according to the logged in user, teacher or student

--- src/router/RouterProvider.jsx
+++ src/router/RouterProvider.jsx
@@ -1,19 +1,19 @@
 import  {BrowserRouter, Routes, Route} from "react-router-dom";
-import TeacherHome from "../home/teacherHome/TeacherHome";
 import Login from '../login/Login'
 import Register from '../login/Register';
 import PrivateRouter from './PrivateRouter';
 import TeacherRouter from "./TeacherRouter";
 import StudentRouter from "./StudentRouter";
+import { useSelector } from "react-redux";
 const RouterProvider = () => {
+const { logged, role } = useSelector((state) => state.login);
   
   return (
     <>
  <BrowserRouter >
     <Routes>
       <Route element={<PrivateRouter />}>
-        < Route path='/*' element={<TeacherRouter />} />
-        < Route path='/*' element={<StudentRouter />} />
+      { role === "teacher" ? < Route path='/*' element={<TeacherRouter />} /> :  < Route path='/*' element={<StudentRouter />} /> }  
       </Route>
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Register />}>
