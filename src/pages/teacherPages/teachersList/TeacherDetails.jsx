d581d90bdfe83e766c01e17c83f305c8165597fb
Enea <gegaenea@yahoo.com>
2024-01-30 02:07:32 +0300

    introduction video added

--- src/pages/teacherPages/teachersList/TeacherDetails.jsx
+++ src/pages/teacherPages/teachersList/TeacherDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { teachers } from '../../../data/teachersList'
 import Navbar from '../../../components/navbar/Navbar'
@@ -7,10 +7,16 @@ import { SubmitButton } from '../../../components/styled/styledbuttons/buttons'
 import { useNavigate } from 'react-router-dom'
 import maleAvatar from "../../../assets/maleAvatar.jpg"
 import femaleAvatar from "../../../assets/femaleAvatar.png"
+import introVideo from "../../../assets/videos/introVideo.webm"
 const TeacherDetails = () => {
   const {id} = useParams()
   const teacher = teachers.find((teacher) => teacher.id === parseInt(id))
   const navigate = useNavigate()
+  const [video, setVideo] = useState(false)
+  const videoHandler = () => {
+   setVideo((prev) => !prev)
+   console.log(video);
+  }
   return (
     <>
       <Navbar />
@@ -19,7 +25,8 @@ const TeacherDetails = () => {
           <img className={styled.img} src={teacher.gender === "Male" ?  maleAvatar : femaleAvatar } alt="" />
           <h3>{ teacher.gender === "Male" ? "Mr. " + teacher.name : "Ms. " + teacher.name }</h3>
           <h3>{teacher.gender}</h3>
-          <h3>{new Date().getFullYear() - teacher.birthyear}</h3>
+          <h3>{new Date().getFullYear() - teacher.birthyear} Years Old</h3>
+          {/* <h3><a href="https://www.youtube.com/watch?v=cWcId6ZoaWs&ab_channel=YanelisaBokveld" target='_blank' >Link</a></h3> */}
           <SubmitButton onClick={() => {
             navigate(-1)
           }} className={styled.goBackBtn}> &#x2190; Go Back </SubmitButton>
@@ -28,8 +35,13 @@ const TeacherDetails = () => {
          <h3>Completed Lessons: </h3>
          <div className={styled.inputDiv}>
           <label>Leave a comment</label>
-          <textarea className={styled.comment} type="text" id='comment' > </textarea>
+          <textarea className={styled.comment} type="text" id='comment'  > </textarea>
           <SubmitButton >Submit</SubmitButton>
+          <div className={styled.introVideo}>
+          {video ? <video width="640" height="360" controls>
+           <source src={introVideo} type="video/webm" />
+         </video> :  <SubmitButton onClick={videoHandler}>Intro Video</SubmitButton> }
+          </div>
          </div>
         </div>
       </div>
