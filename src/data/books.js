13f0346086f0df5cc7ce4079443ea0cfc10b0697
Enea <gegaenea@yahoo.com>
2024-02-09 01:49:10 +0300

    Cart created and population started, price added to the books

--- src/data/books.js
+++ src/data/books.js
@@ -12,112 +12,128 @@ export const books = [
     pbhouse: "Life of Pi",
     author: "Jenny and Jase Mercury",
     freeTrial: true,
-    image: businessBeginner
+    image: businessBeginner,
+    price: 40
 },
 {   id: 2,
     title: "Business English Intermediate ",
     pbhouse: "Life of Pi",
     author: "Jenny and Jase Mercury",
     freeTrial: true,
-    image: businessIntermediate
+    image: businessIntermediate,
+    price: 50
 },
 {   id: 3,
     title: "Business English Advanced ",
     pbhouse: "Life of Pi",
     author: "Jenny and Jase Mercury",
     freeTrial: false,
-    image: businessAdvanced
+    image: businessAdvanced,
+    price: 65
 },
 {   id: 4,
     title: "Spoken English ",
     pbhouse: "HoksFoord Publications",
     author: "Jeremy Jenkins",
     freeTrial: true,
-    image: englishOral
+    image: englishOral,
+    price: 9
 },
 {   id: 5,
     title: "English Phonics",
     pbhouse: "Phoenix House",
     author: "Phillip Philly",
     freeTrial: false,
-    image: englishPhonics
+    image: englishPhonics,
+    price: 25
 },
 {   id: 6,
     title: "English First Grade ",
     pbhouse: "Kampridge",
     author: "Coney McGregor",
     freeTrial: true,
-    image: englishFirstGrade
+    image: englishFirstGrade,
+    price: 18
 },
 {   id: 7,
     title: "English Second Grade ",
     pbhouse: "Kampridge",
     author: "Coney McGregor",
     freeTrial: true,
-    image: englishSecondGrade
+    image: englishSecondGrade,
+    price: 19
 },
 {   id: 8,
     title: "English Third Grade ",
     pbhouse: "Kampridge",
     author: "Coney McGregor",
     freeTrial: true,
-    image: englishThirdGrade
+    image: englishThirdGrade,
+    price: 32
 },
 {    id: 9,
     title: "Business English Beginner ",
     pbhouse: "Life of Pi",
     author: "Jenny and Jase Mercury",
     freeTrial: true,
-    image: businessBeginner
+    image: businessBeginner,
+    price: 35
 },
 {   id: 10,
     title: "Business English Intermediate ",
     pbhouse: "Life of Pi",
     author: "Jenny and Jase Mercury",
     freeTrial: true,
-    image: businessIntermediate
+    image: businessIntermediate,
+    price: 15
 },
 {   id: 11,
     title: "Business English Advanced ",
     pbhouse: "Life of Pi",
     author: "Jenny and Jase Mercury",
     freeTrial: false,
-    image: businessAdvanced
+    image: businessAdvanced,
+    price: 10
 },
 {   id: 12,
     title: "Spoken English ",
     pbhouse: "HoksFoord Publications",
     author: "Jeremy Jenkins",
     freeTrial: true,
-    image: englishOral
+    image: englishOral,
+    price: 50
 },
 {   id: 13,
     title: "English Phonics",
     pbhouse: "Phoenix House",
     author: "Phillip Philly",
     freeTrial: false,
-    image: englishPhonics
+    image: englishPhonics,
+    price: 40
 },
 {   id: 14,
     title: "English First Grade ",
     pbhouse: "Kampridge",
     author: "Coney McGregor",
     freeTrial: true,
-    image: englishFirstGrade
+    image: englishFirstGrade,
+    price: 20
 },
 {   id: 15,
     title: "English Second Grade ",
     pbhouse: "Kampridge",
     author: "Coney McGregor",
     freeTrial: true,
-    image: englishSecondGrade
+    image: englishSecondGrade,
+    price: 25
 },
 {   id: 16,
     title: "English Third Grade ",
     pbhouse: "Kampridge",
     author: "Coney McGregor",
     freeTrial: true,
-    image: englishThirdGrade
+    image: englishThirdGrade,
+    price: 30
 },
 
 ]
--- src/shopping/cart/Cart.jsx
+++ src/shopping/cart/Cart.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import styled from './Cart.module.css'
+import { useSelector } from 'react-redux'
+import { selectCart } from "../../store/index"
 
 const Cart = () => {
+  const cart = useSelector(selectCart)
+  console.log(cart);
   return (
-    <div>Cart</div>
+    <>
+   <div className={styled.container}>
+    <Navbar/>
+    <div className={styled.products} >
+    
+    </div>
+   </div>
+    </>
   )
 }
 
