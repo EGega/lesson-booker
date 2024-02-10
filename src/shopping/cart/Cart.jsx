7493da9c84edcb2dfe063de3576e1e7dd3ad1d92
Enea <gegaenea@yahoo.com>
2024-02-10 04:11:43 +0300

    Cart elements and logic added

--- src/shopping/cart/Cart.jsx
+++ src/shopping/cart/Cart.jsx
@@ -3,16 +3,43 @@ import Navbar from '../../components/navbar/Navbar'
 import styled from './Cart.module.css'
 import { useSelector } from 'react-redux'
 import { selectCart } from "../../store/index"
-
+import { CiCirclePlus } from "react-icons/ci";
+import { CiCircleMinus } from "react-icons/ci";
+import { AiOutlineDelete } from "react-icons/ai";
 const Cart = () => {
   const cart = useSelector(selectCart)
-  console.log(cart);
+  const {selectedBooks} = cart
+  console.log(typeof cart.selectedBooks);
   return (
     <>
    <div className={styled.container}>
     <Navbar/>
     <div className={styled.products} >
-    
+      {
+      selectedBooks && 
+      selectedBooks?.map((el) => {
+        const {title, author, price, image, id } = el
+       return ( 
+       <div className={styled.product} key={id}>
+          <img src={image} className={styled.img} alt="" />
+          <div className={styled.nameAmountDelete}>
+           <h4>{title} by {author}</h4>
+            <div className={styled.incDecDel} >
+              <div className={styled.incDec}>
+               <CiCircleMinus className={styled.dec} />
+               <input  type="number" min="0" max="10" className={styled.number} />
+               <CiCirclePlus className={styled.inc} />
+              </div>
+            <AiOutlineDelete />
+            </div>
+          </div>
+          <h3>{price} $</h3>
+        </div>
+         )    
+      })
+          
+     }
+
     </div>
    </div>
     </>
